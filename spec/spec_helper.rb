6fc4177a576843b97f6a58f58744f9782d8d7324
jasonhutchens <jason@agworld.com.au>
2014-03-28 22:11:17 +0800

    remove reference to debugger

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'sangaku'
-require 'debugger'
 
 
 # Requires supporting files with custom matchers and macros, etc,
